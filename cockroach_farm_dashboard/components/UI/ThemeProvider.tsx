0865928e7f43f6807908c0682d54b6b9c1090393
wende12github <wendmagegn9412@gmail.com>
2026-01-02 19:15:49 +0300

    Change the the component property

--- cockroach_farm_dashboard/components/UI/ThemeProvider.tsx
+++ cockroach_farm_dashboard/components/UI/ThemeProvider.tsx
@@ -3,8 +3,7 @@
 import * as React from 'react'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
 
-type ThemeProviderProps = React.ComponentProps<typeof NextThemesProvider>;
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+export function ThemeProvider({ children, ...props }: React.ComponentProps<typeof NextThemesProvider>) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
